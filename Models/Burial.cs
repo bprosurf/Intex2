8af0da039655d2718c477464ac622d4f25ac57f2
bprosurf <bprosurf@student.byu.edu>
2023-04-14 16:27:27 -0600

    done

--- Models/Burial.cs
+++ Models/Burial.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
+// #nullable disable work
 
 namespace Auth1.Models
 {
